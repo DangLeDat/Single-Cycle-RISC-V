7d994227c4f327e72a9754499fbbf7b4b12daf4e
DangLeDat <dangledat678@gmail.com>
2025-07-10 21:29:56 +0700

    Update IMEM.v

--- IMEM.v
+++ IMEM.v
@@ -2,10 +2,10 @@ module IMEM (
     input [31:0] inst_addr,    // Địa chỉ câu lệnh
     output [31:0] instruction  // Câu lệnh được lấy ra
 );
-    reg [31:0] inst_mem [0:255]; // Bộ nhớ câu lệnh 256 từ
+    reg [31:0] memory [0:255]; // Bộ nhớ câu lệnh 256 từ
 
     // Lấy câu lệnh (địa chỉ căn chỉnh theo từ)
-    assign instruction = inst_mem[inst_addr[31:2]];
+    assign instruction = memory[inst_addr[31:2]];
 
     // In thông tin gỡ lỗi
     always @(*) begin
