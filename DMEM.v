da0cd775c2d812fcf6b236257406536d2aff9a3b
DangLeDat <dangledat678@gmail.com>
2025-07-10 21:30:43 +0700

    Update DMEM.v

--- DMEM.v
+++ DMEM.v
@@ -6,18 +6,18 @@ module DMEM (
     input read_en,             // Tín hiệu cho phép đọc
     output reg [31:0] data_out // Dữ liệu đọc từ bộ nhớ
 );
-    reg [31:0] data_mem [0:255]; // Mảng bộ nhớ 256 từ
+    reg [31:0] memory [0:255]; // Mảng bộ nhớ 256 từ
 
     // Ghi dữ liệu vào bộ nhớ tại cạnh lên của đồng hồ
     always @(posedge clk) begin
         if (write_en)
-            data_mem[mem_addr[31:2]] <= data_in;
+            memory[mem_addr[31:2]] <= data_in;
     end
 
     // Đọc dữ liệu từ bộ nhớ (kết hợp)
     always @(*) begin
         if (read_en)
-            data_out = data_mem[mem_addr[31:2]];
+            data_out = memory[mem_addr[31:2]];
         else
             data_out = 32'h00000000;
     end
