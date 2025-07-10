1813e803e6967f94e9cf177d14e758dbbcac936c
DangLeDat <dangledat678@gmail.com>
2025-07-10 21:45:35 +0700

    Update PC.v

--- PC.v
+++ PC.v
@@ -1,14 +1,14 @@
 module PC (
     input clk,                 // Tín hiệu đồng hồ
-    input rst_n,               // Tín hiệu reset mức thấp
+    input rst_n,               // Reset mức thấp
     input [31:0] next_pc,      // Giá trị PC tiếp theo
-    input pc_en,               // Tín hiệu cho phép cập nhật PC
+    input pc_en,               // Cho phép cập nhật PC
     output reg [31:0] curr_pc  // Giá trị PC hiện tại
 );
-    // Cập nhật PC tại cạnh lên của đồng hồ hoặc khi reset
+    // Cập nhật PC tại cạnh lên đồng hồ hoặc khi reset
     always @(posedge clk or negedge rst_n) begin
         if (!rst_n)
-            curr_pc <= 32'h00000000; // Đặt lại PC về 0
+            curr_pc <= 32'h00000000; // Đặt PC về 0
         else if (pc_en)
             curr_pc <= next_pc;      // Cập nhật PC
     end
